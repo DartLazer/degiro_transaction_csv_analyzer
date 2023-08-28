75c82b9d46454525b1f66121de9234cb5b753a17
DartLazer <71255041+DartLazer@users.noreply.github.com>
2023-08-28 17:15:18 +0200

    Linechart now accepts timestamps in seconds

--- degiro_anaylzer_nuxt/components/LineChart.vue
+++ degiro_anaylzer_nuxt/components/LineChart.vue
@@ -6,20 +6,31 @@
 
 <script setup>
 import ChartJS from 'chart.js/auto';
+
 const {labels, data} = defineProps(['labels', 'data'])
+
+const formattedLabels = labels.map(timestamp => {
+  const date = new Date(timestamp * 1000);
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const year = String(date.getFullYear()).substr(-2);
+
+  return `${month}-${day}-${year}`;
+});
 
 
 const chartRef = ref(null);
 let myChart = null;
 
 const chartData = {
-  labels: labels,
+  labels: formattedLabels,
   datasets: [
     {
       label: 'Total Worth per year',
       backgroundColor: 'rgba(75, 192, 192, 0.2)',
       borderColor: 'rgba(75, 192, 192, 1)',
       borderWidth: 2,
+      cubicInterpolationMode: 'monotone',
       data: data,
       fill: 'origin',
     },
