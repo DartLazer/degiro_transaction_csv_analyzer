ec4dd13887099ce328810d95ef1935f083ff36ce
DartLazer <71255041+DartLazer@users.noreply.github.com>
2023-08-30 12:46:10 +0200

    Code formatting api

--- csv_analyzer_nuxt/services/apiService.js
+++ csv_analyzer_nuxt/services/apiService.js
@@ -10,25 +10,22 @@ export const uploadCsv = async (selectedFile) => {
   const formData = new FormData();
   formData.append('csv_file', selectedFile, selectedFile.name);
 
+  const config = {
+    headers: { 'Content-Type': 'multipart/form-data' },
+    timeout: customTimeout,
+  };
+
   try {
-    const response = await axios.post(`http://${apiAddress}/calculate_multi_year_gain/`, formData, {
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      },
-      timeout: customTimeout,
-    });
+    const response = await axios.post(`http://${apiAddress}/calculate_multi_year_gain/`, formData, config);
     return { data: response.data, error: null };
   } catch (error) {
     let serverErrorMessage;
 
     if (error.response && error.response.data && error.response.data.error) {
-      // Server-side error
       serverErrorMessage = error.response.data.error;
     } else if (error.code === 'ECONNABORTED') {
-      // Timeout error
       serverErrorMessage = 'Request timed out';
     } else {
-      // Network error or problem connecting to the server
       serverErrorMessage = 'Error connecting to server';
     }
 
