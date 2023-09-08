8a37248dfdb60b9964f1781e2b17f8bebcfcdb68
Rik Beernink <rikbeernink@mailbox.org>
2023-09-08 02:24:17 +0200

    Updated front end to not display sections when not needed

--- csv_analyzer_nuxt/components/portFolioDashboard.vue
+++ csv_analyzer_nuxt/components/portFolioDashboard.vue
@@ -9,14 +9,17 @@
           <div class="card-body">
             <div class="padding-left-5">
               <p><strong>Total portfolio worth:</strong> € {{ stockData.summary.total_worth }}</p>
-              <p><strong>Total portfolio gain:  </strong> €
+              <p><strong>Total portfolio gain: </strong> €
                 <span :style="{ color: stockData.summary.total_gain_percentage < 0 ? 'red' : 'green' }"> {{
                     stockData.summary.total_gain_percentage
                   }} % (€ {{ stockData.summary.total_gain }})</span></p>
-              <p><strong>Total Invested:</strong> € {{stockData.summary.total_invested_all_stocks}}</p>
-              <p><strong>Total realized gain:</strong> € {{stockData.summary.total_realized_gain}}</p>
-              <p v-if="stockData.summary.total_realized_profit_loss > 0"><strong>Total realized profit:</strong> € {{stockData.summary.total_realized_profit_loss}}</p>
-              <p><strong>Total Virtual Profit/Loss</strong> {{ (stockData.summary.total_worth + stockData.summary.total_realized_gain) -  stockData.summary.total_invested_all_stocks }}</p>
+              <p><strong>Total Invested:</strong> € {{ stockData.summary.total_invested_all_stocks }}</p>
+              <p><strong>Total realized gain:</strong> € {{ stockData.summary.total_realized_gain }}</p>
+              <p v-if="stockData.summary.total_realized_profit_loss > 0"><strong>Total realized profit:</strong> €
+                {{ stockData.summary.total_realized_profit_loss }}</p>
+              <p><strong>Total Virtual Profit/Loss</strong> {{
+                  (stockData.summary.total_worth + stockData.summary.total_realized_gain) - stockData.summary.total_invested_all_stocks
+                }}</p>
 
               <hr/>
               <LineChart
@@ -43,32 +46,48 @@
               <div class="padding-left-5">
                 <p><strong>Total Invested All Time: </strong>€ {{ stock.total_invested.toFixed(2) }}</p>
                 <p><strong>Realized Gain : </strong>€ {{ stock.realized_gain }}</p>
-                <p v-if="stock.realized_profit_loss > 0 || stock.stocks_in_possession === 0"><strong>Realized profit/loss </strong>€ {{ stock.realized_profit_loss.toFixed(2) }}</p>
-                <hr />
-                <p><strong>Currently Invested: </strong>€ {{ stock.currently_invested.toFixed(2) }}</p>
-                <p><strong>Current Worth: </strong>€ {{ stock.final_worth.toFixed(2) }}</p>
-                <p><strong>Stocks in Possession: </strong>{{ stock.stocks_in_possession }}</p>
+                <p v-if="stock.realized_profit_loss > 0 || stock.stocks_in_possession === 0"><strong>Realized
+                  profit/loss </strong>
+                  <span :style="{ color: stock.realized_profit_loss < 0 ? 'red' : 'green' }">
+                    € {{ stock.realized_profit_loss.toFixed(2) }}
+                  </span></p>
 
-                <p><strong>Virtual Profit / Loss: </strong><span :style="{ color: stock.profit_loss < 0 ? 'red' : 'green' }">
+                <!--- Currently Held Section -->
+                <section id="section_to_display_current_stats" v-if="stock.stocks_in_possession > 0">
+                  <hr/>
+                  <p><strong>Currently Invested: </strong>€ {{ stock.currently_invested.toFixed(2) }}</p>
+                  <p><strong>Current Worth: </strong>€ {{ stock.final_worth.toFixed(2) }}</p>
+                  <p><strong>Stocks in Possession: </strong>{{ stock.stocks_in_possession }}</p>
+
+                  <p><strong>Virtual Profit / Loss: </strong><span
+                      :style="{ color: stock.profit_loss < 0 ? 'red' : 'green' }">
                   € {{ stock.profit_loss }}</span></p>
+                </section>
 
               </div>
-              <hr/>
 
-              <h5 class="padding-left-5">Yearly Performance</h5>
-              <ul>
-                <li v-for="(yearData, year) in stock.yearly_gains" :key="year">
-                  <p class="mt-3">{{ year }} - <span
-                      :style="{ color: yearData.virtual_gain_percentage < 0 ? 'red' : 'green' }">
+              <!---- Section To Display Yearly stats -->
+              <section id="Section to display yearly gains" v-if="stock.yearly_gains">
+                <hr/>
+                <h5 class="padding-left-5">Yearly Performance</h5>
+                <ul>
+                  <li v-for="(yearData, year) in stock.yearly_gains" :key="year">
+                    <p class="mt-3">{{ year }} - <span
+                        :style="{ color: yearData.virtual_gain_percentage < 0 ? 'red' : 'green' }">
                   {{ yearData.virtual_gain_percentage.toFixed(1) }}% (€{{ yearData.virtual_gain_value.toFixed(2) }})
                 </span></p>
-                </li>
-              </ul>
-              <hr/>
-              <LineChart
-                  :labels="Object.keys(stock.yearly_worth)"
-                  :data="Object.values(stock.yearly_worth)"
-              />
+                  </li>
+                </ul>
+              </section>
+
+              <!--- Section to display Line Chart -->
+              <section v-if="!checkIfChartDataEmpty(Object.values(stock.yearly_worth))">
+                <hr/>
+                <LineChart
+                    :labels="Object.keys(stock.yearly_worth)"
+                    :data="Object.values(stock.yearly_worth)"
+                />
+              </section>
             </div>
           </div>
         </div>
@@ -85,6 +104,17 @@ const props = defineProps({
     required: true,
   },
 });
+
+const checkIfChartDataEmpty = function (chartDataValues) {
+  let allValuesZero = true;
+  for (const timestampValue of chartDataValues){
+    if (timestampValue !== 0){
+      return false;
+    }
+  }
+  return allValuesZero
+}
+
 </script>
 
 <style scoped>
